d928456ea7bc287809f191e9752fe9f040ca4c94
NOSI / Estagiario - Elias Evandro Dos Santos Lima <elias.lima@nosi.cv>
2019-12-06 08:51:49 -0100

    atualizacao to page Login

--- src/components/DocSign/login.js
+++ src/components/DocSign/login.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {Form, Button} from 'react-bootstrap'
-import { Container, Row, Col, Nav, Navbar} from 'reactstrap';
+import { Container, Row, Col} from 'reactstrap';
 class login extends Component {
     constructor(props){
         super(props);
@@ -9,17 +9,26 @@ class login extends Component {
  render(){
 
         return(
-       <div>     
-            <Row>  
-                <Container>
-          <Col>   <h1>Log in to DocuSign</h1> </Col>
-
+            
+           
+    <Container> 
+            <Row> 
+                <Col style={{backgroundColor: "lightblue" }}></Col> 
+                <Col> <h1>Log in to DocuSign</h1> </Col>
           <Col>
+ <Row></Row>
+  <form>  
+    <label>Email address</label><br></br>
+    <input type="email" placeholder="Enter email"/><br></br>
+    <label>Password</label><br></br>
+    <input type="password" placeholder="Password"/><br></br>
+    </form> <br></br>
+   <Button className="warning" type="submit"> LOG IN </Button>
    
           </Col>
-    </Container>
-    </Row>
-    </div>
+       </Row>
+ </Container>
+       
         )
         
     }
